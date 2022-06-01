a71806ae365858b9aaf25d2b035524ed5602185d
pablo <pablitoogpde@gmail.com>
2022-06-01 11:47:25 -0300

    Agregar archivos de proyecto.

---
+++ menust/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace menust
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sumaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.restaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiplicacionToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.divisionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.lblresultado = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.operacionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 29);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(79, 25);
+            this.archivoToolStripMenuItem.Text = "archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.salirToolStripMenuItem.Text = "salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // operacionesToolStripMenuItem
+            // 
+            this.operacionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sumaToolStripMenuItem1,
+            this.restaToolStripMenuItem1,
+            this.multiplicacionToolStripMenuItem1,
+            this.divisionToolStripMenuItem});
+            this.operacionesToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operacionesToolStripMenuItem.Name = "operacionesToolStripMenuItem";
+            this.operacionesToolStripMenuItem.Size = new System.Drawing.Size(115, 25);
+            this.operacionesToolStripMenuItem.Text = "operaciones";
+            // 
+            // sumaToolStripMenuItem1
+            // 
+            this.sumaToolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumaToolStripMenuItem1.Name = "sumaToolStripMenuItem1";
+            this.sumaToolStripMenuItem1.Size = new System.Drawing.Size(180, 26);
+            this.sumaToolStripMenuItem1.Text = "sumar";
+            this.sumaToolStripMenuItem1.Click += new System.EventHandler(this.sumaToolStripMenuItem1_Click);
+            // 
+            // restaToolStripMenuItem1
+            // 
+            this.restaToolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restaToolStripMenuItem1.Name = "restaToolStripMenuItem1";
+            this.restaToolStripMenuItem1.Size = new System.Drawing.Size(180, 26);
+            this.restaToolStripMenuItem1.Text = "restar";
+            this.restaToolStripMenuItem1.Click += new System.EventHandler(this.restaToolStripMenuItem1_Click);
+            // 
+            // multiplicacionToolStripMenuItem1
+            // 
+            this.multiplicacionToolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiplicacionToolStripMenuItem1.Name = "multiplicacionToolStripMenuItem1";
+            this.multiplicacionToolStripMenuItem1.Size = new System.Drawing.Size(180, 26);
+            this.multiplicacionToolStripMenuItem1.Text = "multiplicar";
+            this.multiplicacionToolStripMenuItem1.Click += new System.EventHandler(this.multiplicacionToolStripMenuItem1_Click);
+            // 
+            // divisionToolStripMenuItem
+            // 
+            this.divisionToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divisionToolStripMenuItem.Name = "divisionToolStripMenuItem";
+            this.divisionToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.divisionToolStripMenuItem.Text = "dividir";
+            this.divisionToolStripMenuItem.Click += new System.EventHandler(this.divisionToolStripMenuItem_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(185, 140);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(116, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(185, 191);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(116, 20);
+            this.txt2.TabIndex = 2;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(336, 157);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(98, 36);
+            this.lblresultado.TabIndex = 3;
+            this.lblresultado.Text = "resultado";
+            this.lblresultado.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ingrese un numero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ingrese un numero";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sumaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem restaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem multiplicacionToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem divisionToolStripMenuItem;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button lblresultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ menust/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menust
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sumaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txt1.Text);
+            double b= Convert.ToDouble(txt2.Text);
+            double r = a + b;
+            lblresultado.Text = r.ToString();
+
+
+
+        }
+
+        private void restaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txt1.Text);
+            double b = Convert.ToDouble(txt2.Text);
+            double r = a - b;
+            lblresultado.Text = r.ToString();
+
+        }
+
+        private void multiplicacionToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txt1.Text);
+            double b = Convert.ToDouble(txt2.Text);
+            double r = a * b;
+            lblresultado.Text = r.ToString();
+
+        }
+
+        private void divisionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txt1.Text);
+            double b = Convert.ToDouble(txt2.Text);
+            double r = a / b;
+            lblresultado.Text = r.ToString();
+
+        }
+    }
+}
